9a88e616f84f03256a0e1eb314154c8f720c704f
Alina <kotnarow@student.wszib.edu.pl>
2022-11-04 09:05:17 +0100

    Dodano tekst

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,6 +3,8 @@ public class GitTest {
     public static void main(String[] args) {
 
         String tekst = " Próba numer 1111111";
+        String tekst1 = "Próba numer 2";
         System.out.println(tekst);
+        System.out.println(tekst1);
     }
 }
