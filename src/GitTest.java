e2b1981a2a26272f568687f43002779aa857e072
ka7911 <115398298+Kalina7911@users.noreply.github.com>
2022-11-04 08:53:38 +0100

    Zmieniono numer

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,7 +2,7 @@ public class GitTest {
 
     public static void main(String[] args) {
 
-        String tekst = " Próba numer 1";
+        String tekst = " Próba numer 1111111";
         System.out.println(tekst);
     }
 }
